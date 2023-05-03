9c609d13bce938d54e458a64f7b81114755c818b
vicentesannsa <vicentesan155@gmail.com>
2023-05-03 16:19:42 -0400

    Se implemento un router y un formulario. Se espera crear el json para empezar a usar los verbos html

--- index.js
+++ index.js
@@ -2,9 +2,24 @@
 
 const Koa = require('koa');
 const serve = require('koa-static');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
 const app = new Koa();
+const router = new Router();
 
 app.use(serve(`./assets`));
-app.listen(3000);
 
-console.log('Iniciando Koa.JS en http://127.0.0.1:3000/');
+router.get('/', async (ctx, next) => {
+    ctx.body = FileSystem.createReadStream('index.html')
+});
+
+router.post('/recetas', async (ctx, next) => {
+    ctx.body = 'Datos enviados';
+});
+
+app.use(bodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(3000, () => console.log('Iniciando Koa.JS en http://127.0.0.1:3000/'));
