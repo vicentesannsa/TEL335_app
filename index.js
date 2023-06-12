980940fd23351ab708453fa79732efdab5bc0811
vicentesannsa <vicentesan155@gmail.com>
2023-06-11 23:55:11 -0400

    Cambio de estilos, rutas nuevas, se espera nuevamente realizar el post

--- index.js
+++ index.js
@@ -2,23 +2,41 @@
 
 const port = 3001
 
+const fs = require('fs');
 const Koa = require('koa');
 const serve = require('koa-static');
+const path = require('path');
+
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new Router();
 
-app.use(serve(`./assets`));
+const staticPath = path.join(__dirname, 'assets');
+app.use(serve(staticPath));
 
-router.get('/', async (ctx, next) => {
-    ctx.body = FileSystem.createReadStream('index.html')
-});
+app.use(async (ctx) => {
+    if(ctx.path == '/'){
+        ctx.type = 'html';
+        ctx.body = fs.createReadStream(path.join(staticPath, 'index.html'));
+    }
 
-router.post('/recetas', async (ctx, next) => {
-    ctx.body = 'Datos enviados';
-});
+    if(ctx.path == '/cocktail-add'){
+        ctx.type = 'html';
+        ctx.body = fs.createReadStream(path.join(staticPath, 'add.html'));
+    }
+
+    if(ctx.path == '/recetas' && ctx.method == 'POST'){
+        ctx.body = 'Datos enviados';
+        console.log(ctx.path, ctx.method);
+    }
+
+    if(ctx.path == '/enterprise'){
+        ctx.type = 'html';
+        ctx.body = fs.createReadStream(path.join(staticPath, 'enterprise.html'));
+    }
+  });
 
 app.use(bodyParser());
 app.use(router.routes());
