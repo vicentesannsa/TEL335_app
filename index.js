8f4b950368c64ed202ecb4fc2e8722831084ae6b
vicentesannsa <vicentesan155@gmail.com>
2023-07-03 09:19:37 -0400

    Se implemente POST y GET a una ID, falta DELETE y PUT

--- index.js
+++ index.js
@@ -13,13 +13,24 @@ const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 const router = new Router();
 
+app.use(bodyParser());
+
 const staticPath = path.join(__dirname, 'assets');
 app.use(serve(staticPath));
 
 app.use(async (ctx) => {
+    const url = ctx.request.url;
+
     if(ctx.path == '/'){
         ctx.type = 'html';
         ctx.body = fs.createReadStream(path.join(staticPath, 'index.html'));
+    }
+
+    if (url.startsWith('/cocktail/')){
+        
+
+        ctx.type = 'html';
+        ctx.body = fs.createReadStream(path.join(staticPath, 'cocktail.html'));
     }
 
     if(ctx.path == '/cocktail-add'){
@@ -28,8 +39,34 @@ app.use(async (ctx) => {
     }
 
     if(ctx.path == '/recetas' && ctx.method == 'POST'){
-        ctx.body = 'Datos enviados';
-        console.log(ctx.path, ctx.method);
+        const body = ctx.request.body;
+
+        for (const property in body) {
+            if (property == "ingredients" || property == "preparation") {
+                body[property] = body[property].replace(/\r|\n/g, "").split(".")
+                body[property].pop()
+            }
+        }
+
+        try {
+            let options = {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json; charset=utf-8"
+                },
+                body: JSON.stringify(body)
+            }
+
+            res = await fetch("http://localhost:3000/recetas", options),
+            json = await res.json();
+
+            if(!res.ok) throw {status: res.status, statusText: res.statusText};
+            
+            ctx.redirect("/cocktail-add")
+        } catch (err) {
+            console.log(err);
+            ctx.redirect("/")
+        }
     }
 
     if(ctx.path == '/enterprise'){
